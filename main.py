624da3dae1908bda1fae79e984598cc242d49e4b
Christophe Olinger <christophe@olinger.eu>
2025-07-19 12:02:00 +0200

    Review kivy

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from pdf2image import convert_from_bytes
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.image import Image
+
+import time
 
 from pptx import Presentation
 
@@ -77,13 +80,25 @@ class SongCard(
         self.focus = focus
 
 
-class Prompt(BoxLayout):
+class LoadingScreenLayout(BoxLayout):
+    previous_text = ""
 
-    def __init__(self, images = None, number_of_slides=None, **kwargs):
+    def draw_text(self, text):
+        self.ls_text = self.previous_text + "\n" + text
+        self.previous_text = self.previous_text + "\n" + text
+
+
+class HomeLayout(GridLayout):
+    pass
+
+
+class PromptLayout(BoxLayout):
+    drawn_images = []
+    current_image_number = 0
+
+    def __init__(self, images=None, number_of_slides=None, **kwargs):
         super().__init__(**kwargs)
         self.images = images
-        self.drawn_images = []
-        self.current_image_number = 0
         self.number_of_slides = number_of_slides
 
     def draw_first_image(self):
@@ -93,19 +108,7 @@ class Prompt(BoxLayout):
         image = Image(source=self.images[0])
         self.drawn_images.append(image)
         self.add_widget(image)
-    
-    def next_image(self):
-        if self.current_image_number + 1 > self.number_of_slides - 1:
-            return
-        else:
-            for p in self.drawn_images:
-                self.remove_widget(p)
-            image = Image(source=self.images[self.current_image_number + 1])
-            self.drawn_images.append(image)
-            self.add_widget(image)
-            
-            self.current_image_number = self.current_image_number + 1
-    
+
     def prev_image(self):
         if self.current_image_number -1 < 0:
             return
@@ -118,19 +121,37 @@ class Prompt(BoxLayout):
             
             self.current_image_number = self.current_image_number - 1
         
+    def next_image(self):
+        if self.current_image_number + 1 > self.number_of_slides - 1:
+            return
+        else:
+            for p in self.drawn_images:
+                self.remove_widget(p)
+            image = Image(source=self.images[self.current_image_number + 1])
+            self.drawn_images.append(image)
+            self.add_widget(image)
+
+            self.current_image_number = self.current_image_number + 1
 
 
-class TeleprompterMain(FloatLayout):
+class TeleprompterWidget(FloatLayout):
+
+    mode = ObjectProperty("loading")  # Expose to template
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.input_state = None
+        self._input_state = None
+        self._card_instances = None
+        self._placeholders_num = 0
+        
+        self.focused_card = None
 
         # Check for Foot Switch
         self._fs_device = self._find_foot_switch_device()
         threading.Thread(target=self._detect_foot_switch_events, daemon=True).start()
 
-        # If keyboard we need this
+        # Connect Keyboard
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(
             on_key_down=self._on_keyboard_down,
@@ -139,45 +160,12 @@ class TeleprompterMain(FloatLayout):
         self._previous_keyboard_state = None
 
         # Build our UI
-        self.mode = "home"  # home or prompt
-        self.focused_card = None
-        self.card_instances = None
-        self.cards = self._load_songbook()
-        self.placeholders_num = 0
-        self._initialize_ui()
+        # Window.fullscreen = True
+        Window.allow_screensaver = False
 
-    def _load_songbook(self):
-        songbook_folder = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "songbook"
-        )
-        cards = []
-        if os.path.exists(songbook_folder):
-            for f in sorted(os.listdir(songbook_folder)):
-                if f.startswith("~"):
-                    continue  
-                if f.endswith("pptx"):
-                    info = f.replace(".pptx", "")
-                    number_of_slides = self._number_of_slides(os.path.join(songbook_folder, f))
-                    card = {
-                        "sequence": info.split("-")[0].strip(),
-                        "artist": info.split("-")[1].strip(),
-                        "song": info.split("-")[2].strip(),
-                        "images": self._presentation_to_images(
-                            os.path.join(songbook_folder, f),
-                            number_of_slides,
-                        ),
-                        "number_of_slides": number_of_slides
-                    }
-                    cards.append(card)
-        if len(cards) == 0:
-            return [
-                {"sequence": "1", "artist": "A", "song": "Hallo"},
-                {"sequence": "2", "artist": "B", "song": "Me"},
-                {"sequence": "3", "artist": "C", "song": "Too"},
-                {"sequence": "4", "artist": "D", "song": "Sing"},
-            ]
-        else:
-            return cards
+    """
+    Setup UI
+    """
 
     def _find_foot_switch_device(self):
         fs_device = None
@@ -219,32 +207,32 @@ class TeleprompterMain(FloatLayout):
                         state = "hold"
 
                     if btn and state:
-                        self.input_state = (btn, state)
+                        self._input_state = (btn, state)
                         self._decide_action()
 
     def _keyboard_closed(self):
         # Do not unbind, otherwise escape from prompt will switch to home but then no keys are detected anymore
         pass
-        #self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        #self._keyboard = None
+        # self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        # self._keyboard = None
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         key = keycode[1]
         btn = None
         state = None
-        if key == "1" or key == "numapd1":
+        if key == "1" or key == "numpad1":
             btn = "KEY_A"
             if self._previous_keyboard_state == "down" + btn:
                 state = "hold"
             else:
                 state = "down"
-        if key == "2" or key == "numapd2":
+        if key == "2" or key == "numpad2":
             btn = "KEY_B"
             if self._previous_keyboard_state == "down" + btn:
                 state = "hold"
             else:
                 state = "down"
-        if key == "3" or key == "numapd3":
+        if key == "3" or key == "numpad3":
             btn = "KEY_C"
             if self._previous_keyboard_state == "down" + btn:
                 state = "hold"
@@ -263,7 +251,7 @@ class TeleprompterMain(FloatLayout):
                 )
 
         if btn and state:
-            self.input_state = (btn, state)
+            self._input_state = (btn, state)
             self._previous_keyboard_state = state + btn
             self._decide_action()
         return True
@@ -282,104 +270,92 @@ class TeleprompterMain(FloatLayout):
             btn = "KEY_C"
             state = "up"
         if btn and state:
-            self.input_state = (btn, state)
+            self._input_state = (btn, state)
             # self._decide_action()
-        if key == "escape":
-            # Stop listener
-            keyboard.release()
         return True
 
     def _decide_action(self):
-        if self.input_state:
+        if self._input_state:
             if self.mode == "home":
-                if self.input_state[0] == "KEY_A" and self.input_state[1] in [
+                if self._input_state[0] == "KEY_A" and self._input_state[1] in [
                     "hold",
                     "down",
                 ]:
                     self.focus_previous_card()
-                if self.input_state[0] == "KEY_C" and self.input_state[1] in [
+                if self._input_state[0] == "KEY_C" and self._input_state[1] in [
                     "hold",
                     "down",
                 ]:
                     self.focus_next_card()
 
-                if self.input_state[0] == "KEY_B" and self.input_state[1] in [
+                if self._input_state[0] == "KEY_B" and self._input_state[1] in [
                     "hold",
                     "down",
                 ]:
                     self.enter_prompt()
             else:
-                if self.input_state[0] == "KEY_A" and self.input_state[1] in [
+                if self._input_state[0] == "KEY_A" and self._input_state[1] in [
                     "hold",
                     "down",
                 ]:
-                    self.prompt_widget.prev_image()
-                if self.input_state[0] == "KEY_C" and self.input_state[1] in [
+                    self.prompt_prev()
+                if self._input_state[0] == "KEY_C" and self._input_state[1] in [
                     "hold",
                     "down",
                 ]:
-                    self.prompt_widget.next_image()
+                    self.prompt_next()
+                    self.ids["prompt_layout"].next_image()
 
-                if self.input_state[0] == "KEY_B" and self.input_state[1] in [
+                if self._input_state[0] == "KEY_B" and self._input_state[1] in [
                     "hold",
                     "down",
                 ]:
                     self.set_mode("home")
 
-    def _initialize_ui(self):
+    """
+    Actions
+    """
 
-        # Window level
-        Window.fullscreen = True
-        Window.allow_screensaver = False
+    def focus_previous_card(self):
+        next_index = self.focused_card.index - 1
+        if next_index < 0:
+            next_index = len(self._card_instances) - 1 - self._placeholders_num
+        for c in self._card_instances:
+            if c.index == next_index:
+                c.set_focus(True)
+                self.focused_card = c
+            else:
+                c.set_focus(False)
 
-        # Main box (Create Grid for Home and Box for Prompt)
-        self.home_layout = GridLayout(
-            cols=HOME_MIN_COLS_NUM,
-            rows=HOME_MIN_ROWS_NUM,
-            spacing=10,
-            padding=10,
-            orientation="lr-tb",
-        )
-        self.prompt_layout = BoxLayout(opacity=0)
-        self.add_widget(self.home_layout)
-        self.add_widget(self.prompt_layout)
+    def focus_next_card(self):
+        next_index = self.focused_card.index + 1
+        if next_index >= len(self._card_instances) - self._placeholders_num:
+            next_index = 0
+        for c in self._card_instances:
+            if c.index == next_index:
+                c.set_focus(True)
+                self.focused_card = c
+            else:
+                c.set_focus(False)
 
-        # Fill home
-        # Do we need placeholders
-        if len(self.cards) < HOME_MIN_COLS_NUM * HOME_MIN_ROWS_NUM:
-            to_add = HOME_MIN_COLS_NUM * HOME_MIN_ROWS_NUM - len(self.cards)
-            self.placeholders_num = to_add
-            while to_add > 0:
-                self.cards.append(
-                    {
-                        "is_placeholder": True,
-                        "sequence": None,
-                        "artist": None,
-                        "song": None,
-                    }
-                )
-                to_add = to_add - 1
+    def enter_prompt(self):
+        self.ids["prompt_layout"].images = self.focused_card.images
+        self.ids["prompt_layout"].number_of_slides = self.focused_card.number_of_slides
+        self.ids["prompt_layout"].draw_first_image()
+        self.set_mode("prompt")
 
-        # Create card widgets and place into grid
-        self.card_instances = []
-        for index, c in enumerate(self.cards):
-            c_instance = SongCard(
-                sequence=c["sequence"],
-                artist=c["artist"],
-                song=c["song"],
-                images=c.get("images", []),
-                is_placeholder=c.get("is_placeholder", False),
-                number_of_slides=c.get("number_of_slides"),
-                index=index,
-            )
-            self.home_layout.add_widget(c_instance)
-            self.card_instances.append(c_instance)
-        self.card_instances[0].set_focus()
-        self.focused_card = self.card_instances[0]
+    def prompt_prev(self):
+        self.ids["prompt_layout"].prev_image()
 
-        # Fill Prompt
-        self.prompt_widget = Prompt()
-        self.prompt_layout.add_widget(self.prompt_widget)
+    def prompt_next(self):
+        self.ids["prompt_layout"].next_image()
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+    """
+    Load Song Book
+    """
 
     def _number_of_slides(self, path_to_presentation):
         prs = Presentation(path_to_presentation)
@@ -391,27 +367,31 @@ class TeleprompterMain(FloatLayout):
         if not os.path.exists(OUT_DIR):
             os.mkdir(OUT_DIR)
 
-        ###start = time.time()
-        print("Start converting your PPT to {} images.".format(IMAGE_FORMAT))
-
+        ### start = time.time()
+        message = "Converting {}.".format(os.path.basename(ppt_path))
+        self.ids["ls_screen"].draw_text(message)
         filename_bare = os.path.basename(ppt_path).replace(".pptx", "")
-        
+
         # Give cache if images are present but only if ppt is not newer
         expected_image_paths = []
         cache_ok = True
         for i in range(num_slides):
-            expected_image = os.path.join(OUT_DIR, f"{filename_bare}-{i}.{IMAGE_FORMAT}")
+            expected_image = os.path.join(
+                OUT_DIR, f"{filename_bare}-{i}.{IMAGE_FORMAT}"
+            )
             expected_image_paths.append(expected_image)
             if os.path.exists(expected_image) is False:
                 cache_ok = False
             else:
                 if os.path.getmtime(expected_image) < os.path.getmtime(ppt_path):
                     cache_ok = False
+                    self.ids["ls_screen"].draw_text("cache obsolete")
         if cache_ok:
-            print("Taking cached image")
+            self.ids["ls_screen"].draw_text("taking from cache")
             return expected_image_paths
 
         # convert pptx to PDF
+        self.ids["ls_screen"].draw_text("converted")
         command_list = ["soffice", "--headless", "--convert-to", "pdf", ppt_path]
         subprocess.run(command_list)
 
@@ -426,51 +406,92 @@ class TeleprompterMain(FloatLayout):
             created_image_paths.append(im_name)
             img.save(im_name)
         os.unlink(pdffile_name)
-        
+
         return created_image_paths
 
-    def focus_previous_card(self):
-        next_index = self.focused_card.index - 1
-        if next_index < 0:
-            next_index = len(self.card_instances) - 1 - self.placeholders_num
-        for c in self.card_instances:
-            if c.index == next_index:
-                c.set_focus(True)
-                self.focused_card = c
-            else:
-                c.set_focus(False)
-
-    def focus_next_card(self):
-        next_index = self.focused_card.index + 1
-        if next_index >= len(self.card_instances) - self.placeholders_num:
-            next_index = 0
-        for c in self.card_instances:
-            if c.index == next_index:
-                c.set_focus(True)
-                self.focused_card = c
-            else:
-                c.set_focus(False)
-
-    def enter_prompt(self):
-        self.prompt_widget.images = self.focused_card.images
-        self.prompt_widget.number_of_slides = self.focused_card.number_of_slides
-        self.prompt_widget.draw_first_image()
-        self.set_mode("prompt")
-
-    def set_mode(self, mode):
-        if mode == "prompt":
-            self.mode = "prompt"
-            self.home_layout.opacity = 0
-            self.prompt_layout.opacity = 1
+    def load_songbook(self):
+        songbook_folder = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "songbook"
+        )
+        cards = []
+        if os.path.exists(songbook_folder):
+            for f in sorted(os.listdir(songbook_folder)):
+                if f.startswith("~"):
+                    continue
+                if f.endswith("pptx"):
+                    info = f.replace(".pptx", "")
+                    number_of_slides = self._number_of_slides(
+                        os.path.join(songbook_folder, f)
+                    )
+                    card = {
+                        "sequence": info.split("-")[0].strip(),
+                        "artist": info.split("-")[1].strip(),
+                        "song": info.split("-")[2].strip(),
+                        "images": self._presentation_to_images(
+                            os.path.join(songbook_folder, f),
+                            number_of_slides,
+                        ),
+                        "number_of_slides": number_of_slides,
+                    }
+                    cards.append(card)
+        if len(cards) == 0:
+            return [
+                {"sequence": "1", "artist": "A", "song": "Hallo"},
+                {"sequence": "2", "artist": "B", "song": "Me"},
+                {"sequence": "3", "artist": "C", "song": "Too"},
+                {"sequence": "4", "artist": "D", "song": "Sing"},
+            ]
         else:
-            self.mode = "home"
-            self.home_layout.opacity = 1
-            self.prompt_layout.opacity = 0
+            return cards
+
+    def initialize_home(self):
+
+        # Do we need placeholders ?
+        if len(self.cards) < HOME_MIN_COLS_NUM * HOME_MIN_ROWS_NUM:
+            to_add = HOME_MIN_COLS_NUM * HOME_MIN_ROWS_NUM - len(self.cards)
+            self._placeholders_num = to_add
+            while to_add > 0:
+                self.cards.append(
+                    {
+                        "is_placeholder": True,
+                        "sequence": None,
+                        "artist": None,
+                        "song": None,
+                    }
+                )
+                to_add = to_add - 1
+
+        # Create card widgets
+        self._card_instances = []
+        for index, c in enumerate(self.cards):
+            c_instance = SongCard(
+                sequence=c["sequence"],
+                artist=c["artist"],
+                song=c["song"],
+                images=c.get("images", []),
+                is_placeholder=c.get("is_placeholder", False),
+                number_of_slides=c.get("number_of_slides"),
+                index=index,
+            )
+            self._card_instances.append(c_instance)
+            self.ids["home_layout"].add_widget(c_instance)
+
+        # Focus first card
+        self._card_instances[0].set_focus()
+        self.focused_card = self._card_instances[0]
 
 
 class TeleprompterApp(App):
+
+    # Expose constants to kivy
+    HOME_MIN_ROWS_NUM = HOME_MIN_ROWS_NUM
+    HOME_MIN_COLS_NUM = HOME_MIN_COLS_NUM
+
     def build(self):
-        main = TeleprompterMain()
+        main = TeleprompterWidget()
+        main.cards = main.load_songbook()
+        main.initialize_home()
+        main.set_mode("home")
         return main
 
 
