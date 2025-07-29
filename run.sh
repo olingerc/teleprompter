d5cc9fc265a2c415e9742b3d8be62f4927658ff4
Christophe Olinger <christophe@olinger.eu>
2025-07-29 20:17:50 +0200

    Add automatic git pull and run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+parentdir="$(dirname "$script_dir")"
+
+# Git pull songbook
+echo "Pulling new songbooks in SONGBOOK in ${parentdir}/songbooks"
+cd "${parentdir}/songbooks"
+git pull
+
+# Git pull app and run
+echo "Starting Telepromter in ${script_dir}"
+cd "${script_dir}"
+git pull
+$script_dir/.venv/bin/python3 $script_dir/main.py
